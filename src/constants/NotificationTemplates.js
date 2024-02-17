39693738b86cc4f21f8a8526ffe01cec248a425c
Sudarshan Sharma <sudarshansharmano.1@gmail.com>
2024-02-17 17:50:11 +0530

    Added predefined templates for email ans SMS

---
+++ src/constants/NotificationTemplates.js
@@ -0,0 +1,8 @@
+const SMS_TEMPLATE = 'Hi! We have exciting offer exclusive for you. Please contact us at sudarshansharmano.1@gmail.com'
+const EMAIL_TEMPLATE = 'Hi! We have exciting offer exclusive for you. Please contact us at sudarshansharmano.1@gmail.com'
+
+
+module.exports = {
+    SMS_TEMPLATE,
+    EMAIL_TEMPLATE
+}
